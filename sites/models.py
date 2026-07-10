da1608c0fe098d6532e9933a2fa5e15b3afeec6b
achibhossengit <mail.achibhossen@gmail.com>
2026-07-10 22:28:46 +0600

    add is_closed field with site model

--- sites/models.py
+++ sites/models.py
@@ -17,6 +17,7 @@ def default_present_choices():
 class Site(TimeStampedMixin, CompanyOwnedMixin, CreatedByMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
+    is_closed = models.BooleanField(default=False)
     closed_at = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
