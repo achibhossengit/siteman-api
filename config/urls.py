572f23e16911167ee686c721a0a0d37f68c24b51
achibhossengit <mail.achibhossen@gmail.com>
2026-08-21 15:09:24 +0600

    chore: expose OpenAPI docs only when DEBUG

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,6 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,9 +8,17 @@ urlpatterns = [
     path('api/<str:version>/', include('sites.urls')),
     path('api/<str:version>/', include('labours.urls')),
     path('api/<str:version>/', include('activity.urls')),
-
-    # OpenAPI schema + docs
-    path('api/schema/', SpectacularAPIView.as_view(api_version='v1'), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
+
+if settings.DEBUG:
+    from drf_spectacular.views import (
+        SpectacularAPIView,
+        SpectacularRedocView,
+        SpectacularSwaggerView,
+    )
+
+    urlpatterns += [
+        path('api/schema/', SpectacularAPIView.as_view(api_version='v1'), name='schema'),
+        path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+        path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    ]
