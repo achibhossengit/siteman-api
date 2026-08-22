1a47e9389bc35e899a7bbaf91143ad879b01e13a
achibhossengit <mail.achibhossen@gmail.com>
2026-08-22 09:09:50 +0600

    refactor(admin): set default list_per_page to 20

--- core/apps.py
+++ core/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "core"
+
+    def ready(self):
+        from django.contrib.admin.options import ModelAdmin
+
+        ModelAdmin.list_per_page = 20
