88bb017fa9d92e3977bb1fdb60ce2561138c024b
achibhossengit <mail.achibhossen@gmail.com>
2026-09-07 09:22:58 +0600

    feat(config): redirect root URL to API docs

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
@@ -8,6 +9,7 @@ from drf_spectacular.views import (
 )
 
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='swagger-ui', permanent=False)),
     path(settings.ADMIN_URL, admin.site.urls),
     path('api/<str:version>/', include('accounts.urls')),
     path('api/<str:version>/', include('company.urls')),
